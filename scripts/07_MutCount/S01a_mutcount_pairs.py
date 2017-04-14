4d48f83c4c8b12abca0f3efc63294b2983a9db79
Mataivic <victor.mataigne@wanadoo.fr>
2017-04-14 10:46:20 +0200

    New brick : MutCount (scripts, symlinks, wrapper with functional test ; in progress

---
+++ scripts/07_MutCount/S01a_mutcount_pairs.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+#
+
+import os, itertools,sys
+
+
+list_species=[]
+num_sampled= sys.argv[1]
+num_iter=sys.argv[2]
+list_species= sys.argv[3]
+list_species=list_species.split(",")
+pairs_list=list(itertools.combinations(list_species,2))
+print pairs_list
+
+pairs_file=open("pairs.txt","w")
+pairs_file.write("background: %s %s + all\n"%(num_sampled,num_iter))
+for pair in pairs_list:
+	pairs_file.write("%s %s\n"%(pair[0],pair[1]))
+pairs_file.close()
+
+
+
+
+
