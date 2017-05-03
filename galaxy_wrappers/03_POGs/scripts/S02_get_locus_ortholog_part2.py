2914e872c62a5777604ca23357c49360615ec1b6
Mataivic <victor.mataigne@wanadoo.fr>
2017-05-03 14:38:14 +0200

    updated pogs with paralogs filtering option

--- galaxy_wrappers/03_POGs/scripts/S02_get_locus_ortholog_part2.py
+++
@@ -1,1 +0,0 @@
-../../../scripts/03_POGs/S02_get_locus_ortholog_part2.py
---
+++ galaxy_wrappers/03_POGs/scripts/get_locus_orthologs_part1_v2.py
@@ -0,0 +1,1 @@
+/home/umr7144/abice/vmataigne/Documents/Fork_AdaptSearch/adaptsearch/scripts/03_POGs/get_locus_orthologs_part1_v2.py
