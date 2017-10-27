9fe0aed169a255e18e5e9d8e9628387d69ab444b
Mataivic <victor.mataigne@wanadoo.fr>
2017-10-27 15:24:41 +0200

    Applied suggestions

--- scripts/03b_Orthogroups_Tool/format_transdecoder_headers.sh
+++ scripts/03b_Orthogroups_Tool/format_transdecoder_headers.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
-# This script contains regex to re-write the outputs of transdecoder to the original AdaptSearch format 
+
+#This script contains regex to re-write the outputs of transdecoder to the original AdaptSearch format 
+#Example :
+#OG0007971: m.35 g.35  ORF g.35 m.35 type_internal len_307 _+_ Th132_1/1_1.000_923_1-924_+_
+#Becomes :
+#Th132_1/1_1.000_923
 
 # removes 'OGxxxxxxx '
 sed -i -E 's/OG[0-9]{7}:\s//' $1 
@@ -12,6 +17,3 @@ sed -i -E 's/m\.[0-9]{1,}[^()]+\(\+\)\s*/>/g' $1
 sed -i 's/(+)//g' $1
 # Removes last suite of unwanted numbers, underscore and dash
 sed -i -E 's/\_[0-9]{1,}-[0-9]{1,}//g' $1
-
-# Other way to do - does not work
-#sed -i -E 's/_[0-9]{1,}-[0-9]{1,}\(\+\)//g' $1
