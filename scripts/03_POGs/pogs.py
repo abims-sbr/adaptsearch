fec303a87261120575cc3329182d423ecfaca1e9
Mataivic <victor.mataigne@wanadoo.fr>
2018-01-04 10:01:25 +0100

    Improvments with argparse

--- scripts/03_POGs/pogs.py
+++ scripts/03_POGs/pogs.py
@@ -4,8 +4,14 @@
 # Command line : ./pogsPOO.py <list_of_input_files_separated_by_commas> <minimal number of species per group>
 
 """
+What it does:
+    - pogs.py parses output files from the "pairwise" tool of the AdaptSearch suite and proceeds to gather genes in orthogroups (using transitivity).
+    - A minimal number of species per group can be set.
+
 Improvments to do :
     - Paralogous filtering /removal is still to do
+
+Errors corrected (need to test a bit more to be sure)
     - makeOrthogroups is still incorrect
         - ERROR 1
             - Pour peu qu'une paire soit associee au groupe grace a une paire qui sera parcourue après elle, cette premiere paire n'est pas ajoutee
@@ -16,7 +22,7 @@ Improvments to do :
                 On dirait qu'il faut créer une classe Orthogroup qui hérite de set et redéfinir la méthode intersection ...
 """
 
-import sys, os
+import os, argparse
 
 class Locus:
     """ Definition of a locus : header + sequence """
@@ -46,10 +52,10 @@ class Locus:
 
     def prettyPrint(self):
         # Used for debugging : print "{ Header : ", self.header[0:-1], "Tag : ", self.tagged, " }"
-        print "{ Header : ", self.header[0:-1], " }"
+        print "[ Header : {header} ]".format(header=self.header[0:-1])
 
     def prettyPrint2(self):        
-        print "{ Header : ", self.header [0:-1], " Sequence : ", self.sequence[0:-1], " }"
+        print "[ Header : {header} Sequence : {sequence} ]".format(header=self.header[0:-1], sequence=self.sequence[0:-1])
 
 def getListPairwiseAll(listPairwiseFiles):
     """ Applies the getPairwiseCouple() function to a list of files and return a big list with ALL pairwises couples """
@@ -59,19 +65,9 @@ def getListPairwiseAll(listPairwiseFiles):
         """ Reads an output file from the 'Pairwise' tool (AdaptSearch suite) and returns its content into a list """
         list_pairwises_2sp = []
         with open(pairwiseFile, "r") as file:
-            while (1):  # Ugly code !
+            while (1):  # Ugly !
                 name, sequence, name2, sequence2 = file.readline(), file.readline(), file.readline(), file.readline()
-                if not name: break            
-                """
-                if name[-1]=="\n":
-                    name=name[0:-1]
-                if sequence[-1]=="\n":
-                    sequence=sequence[0:-1]
-                if name2[-1]=="\n":
-                    name2=name2[0:-1]
-                if sequence2[-1]=="\n":
-                    sequence2=sequence2[0:-1]
-                """
+                if not name: break # Use assert ?
                 # One locus every two lines (one pairwise couple = 4 lines) : header + sequence
                 locus1 = Locus(name, sequence)
                 locus2 = Locus(name2, sequence2)
@@ -89,7 +85,7 @@ def getListPairwiseAll(listPairwiseFiles):
             list_pairwises_allsp.append(pairwise)  # all pairwises in the same list
     return list_pairwises_allsp
 
-def makeOrthogroups(list_pairwises_allsp):
+def makeOrthogroups(list_pairwises_allsp, minspec, args):
     """ Proceeds to create orthogroups by putting together pairwise couples sharing a locus.
      Iterates over the orthogroups list and tag to 'True' the pairwise couple already gathered in a group to avoid
      redondancy. Writes each orthogroup in a fasta file"""
@@ -99,7 +95,7 @@ def makeOrthogroups(list_pairwises_allsp):
         """ Check if a locus/group has already been treated in makeOrthogroups()"""
         tag = True
         for element in pair:        
-            if not element.getTag() and tag:
+            if not element.getTag() and tag: # use a list comprehension maybe ?
                 tag = False
         return tag
 
@@ -111,7 +107,7 @@ def makeOrthogroups(list_pairwises_allsp):
 
     def writeOutputFile(orthogroup, number):
         """ Write an orthogroup in a file """
-        name = "orthogroup_%d_with_%d_sequences.fasta" % (number, len(orthogroup))
+        name = "orthogroup_{}_with_{}_sequences.fasta".format(number, len(orthogroup))
         result = open(name, "w")
         with result:
             for locus in orthogroup:            
@@ -123,7 +119,7 @@ def makeOrthogroups(list_pairwises_allsp):
                     result.write("%s" % locus.getSequence())  # write sequence
                 else :
                     result.write("%s\n" % locus.getSequence())  # write sequence
-        os.system("mv %s outputs/" % name)
+        os.system("mv {} outputs/".format(name))
 
     # Function
     list_orthogroups = []
@@ -147,28 +143,39 @@ def makeOrthogroups(list_pairwises_allsp):
 
             if len(orthogroup) > 2:
                 tagGroup(ortho_pair1) # potentially useless ?
-                if len(orthogroup) >= int(sys.argv[2]):
+                if len(orthogroup) >= minspec:
                     list_orthogroups.append(orthogroup)                
                     writeOutputFile(orthogroup, i)
                     i += 1
-
-    for group in list_orthogroups:
-        print "*** Orthogroup with %s sequences ***" %len(group)
-        for locus in group:
-            locus.prettyPrint()
-        print ""
+    
+    # print groups in the log file
+    if args.verbose > 0:
+        for group in list_orthogroups:
+            print "*** Orthogroup with {} sequences ***".format(len(group))
+            for locus in group:
+                if args.verbose == 1 :
+                    locus.prettyPrint()
+                elif args.verbose == 2 :
+                    locus.prettyPrint2()
+            print ""
     
     return len(list_orthogroups)
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", help="Input files separated by commas. Each file contains all the reciprocical best hits between a pair of species")
+    parser.add_argument("minspec", help="Only keep Orthogroups with at least this number of species", type=int)
+    parser.add_argument("-v", "--verbose", type=int, choices=[0,1,2], help="Content of orthogroups will be displayed on the screen")
+    args = parser.parse_args()
+
     os.system("mkdir outputs")
-    infiles = sys.argv[1]
+    infiles = args.files
     listPairwiseFiles = str.split(infiles, ",")
     print "Parsing input files ..."
     list_Locus = getListPairwiseAll(listPairwiseFiles)
     print "Creating Orthogroups ..."
-    nb_orthogroups = makeOrthogroups(list_Locus)
-    print "%d orthogroups have been infered from %d pairwise comparisons by RBH" % (nb_orthogroups, len(listPairwiseFiles))
+    nb_orthogroups = makeOrthogroups(list_Locus, args.minspec, args) # args must be passed as a parameter ; maybe not the best way to do it ?
+    print "{} orthogroups have been infered from {} pairwise comparisons by RBH".format(nb_orthogroups, len(listPairwiseFiles))
 
 if __name__ == "__main__":
     main()
