0625a16e524eb8689a17968651a9ff100e78eea1
Mataivic <victor.mataigne@wanadoo.fr>
2018-07-06 08:45:45 +0200

    quick bugfix

--- scripts/02_Pairwise/S01_run_first_blast.py
+++ scripts/02_Pairwise/S01_run_first_blast.py
@@ -34,7 +34,7 @@ def main():
             translated_file = open(name, 'w')
             with open(file, 'r') as file:
                 for name, seq in itertools.izip_longest(*[file]*2):
-                    s = Seq(seq.strip('\n').upper(), IUPAC.unambiguous_dna)
+                    s = Seq(seq.strip('\n').upper(), IUPAC.ambiguous_dna)
                     translated_file.write(name.strip('\n')+'_orf_1\n')
                     translated_file.write(s.translate()._data+'\n')
                     translated_file.write(name.strip('\n')+'_orf_2\n')
@@ -62,7 +62,7 @@ def main():
             print "Pair of species:"
             print pairwise
 
-            sp1, sp2 = pairwise[0].split('_')[1], pairwise[1].split('_')[1]
+            sp1, sp2 = pairwise[0].split('_')[1], pairwise[1].split('_')[1] #rename 'translated_Xx_transcriptom.fasta'
             sub_directory_name = sp1 + '_' + sp2
             os.mkdir('./blast_%s' %sub_directory_name)
 
